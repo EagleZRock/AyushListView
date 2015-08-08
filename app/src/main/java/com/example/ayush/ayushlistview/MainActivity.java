a15576bda4d60f0ae0056381757f3a95fb3ba0c0
EagleZRock <ayush.viswanathan@gmail.com>
2015-08-08 18:14:28 -0400

    Ayush Commit for Finishing List View Assignment

--- app/src/main/java/com/example/ayush/ayushlistview/MainActivity.java
+++ app/src/main/java/com/example/ayush/ayushlistview/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.ayush.ayushlistview;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,7 +17,38 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Creating a array of the list of birds
+        String[] birdList = {"peacock", "sparrow", "falcon", "eagle", "cardinal"};
+        //Looking for the id of the list view we created in activity_main.xml
+        final ListView birdListView = (ListView) findViewById(R.id.bird_list_view);
+
+        //Creating an adapter for the array and giving it parameters (it is a constructor)
+        ArrayAdapter<String> birdListAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,
+                android.R.id.text1, birdList);
+
+        //Setting the adapter to the List View we created
+        birdListView.setAdapter(birdListAdapter);
+
+        //Creating an onClickListener for the List View
+        ListView.OnItemClickListener birdListViewItemClickListener = new ListView.OnItemClickListener() {
+            @Override
+            //Invoking method called onItemClick with parameters
+            //I believe that this is allow the item to be clicked on
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                int itemPosition = position;
+
+                String itemValue = (String) birdListView.getItemAtPosition(itemPosition);
+
+            }
+
+        };
+        //Setting onItemClickListener to birdList View
+        birdListView.setOnItemClickListener(birdListViewItemClickListener);
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
